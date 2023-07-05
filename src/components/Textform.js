3bfd29dc2fa8c482d2f14ffa95040ff971c49eea
Suraj Paul <71144722+surajpaul28@users.noreply.github.com>
2023-07-05 22:29:16 +0530

    Textform.js

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -3,43 +3,39 @@ import React, {useState} from "react";
 export default function Textform(props) {
   const onClickUpper=()=>{
     //console.log("Uppercase was clicked"+ text);
-    let newText= textfirst.toUpperCase();
-    setTextfirst(newText)
+    let newText= text.toUpperCase();
+    setText(newText)
   }
   const OnchangeUpper=(event)=>{
     //console.log("On change");
-    setTextfirst(event.target.value);
+    setText(event.target.value);
   }
 
   //to lowercase
   const onClicklower=()=>{
     //console.log("Uppercase was clicked"+ text);
-    let lowerText= textsecond.toLowerCase();
-    setTextsecond(lowerText)
+    let lowerText= text.toLowerCase();
+    setText(lowerText)
   }
  
-  //to lowercase
-  const Onchangelower=(event)=>{
-    //console.log("On change");
-    setTextsecond(event.target.value);
-  }
-    const [textfirst, setTextfirst] = useState('Enter text here');
-    const [textsecond, setTextsecond] = useState('Enter text here');
+    const [text, setText] = useState('Enter text here');
   return (
+    <>
     <div>
         <h1>{props.heading}</h1>
       <div className="mb-3">
-        <label for="myBox" className="form-label">Enter text to change it in uppercase.</label>
-        <textarea className="form-control" value={textfirst} onChange={OnchangeUpper} id="myBox" rows="6"></textarea>
+        <label for="myBox" className="form-label">Enter text to change it in uppercase or lowercase</label>
+        <textarea className="form-control" value={text} onChange={OnchangeUpper} id="myBox" rows="10"></textarea>
       </div>
       <button className="button" onClick={onClickUpper}>Convert to Uppercase</button>
-    <div>
-    <div className="mb-3">
-        <label for="myBox" className="form-label">Enter text to change it in lowercase.</label>
-        <textarea className="form-control" value={textsecond} onChange={Onchangelower} id="myBox" rows="6"></textarea>
-      </div>
       <button className="button" onClick={onClicklower}>Convert to Lowercase</button>
     </div>
+    <div className="container">
+      <h3>Text summary</h3>
+      <p>{text.split(" ").length} words and {text.length} characters.</p>
+      <p>{0.008 * text.split(" ").length} Minutes reading time.</p>
     </div>
+    </>
+    
   );
 }
